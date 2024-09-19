9a80b00b6556ff066b1cc2dd546081923a6292e2
Juliano de Medeiros Machado <julianomachado@protonmail.com>
2024-09-19 00:31:16 -0300

    Improvement on prompt

--- handlers/resumo.py
+++ handlers/resumo.py
@@ -9,11 +9,11 @@ genai.configure(api_key=GOOGLE_API_KEY)
 model = genai.GenerativeModel('gemini-1.5-flash')
 
 async def resumo_handler(message: types.Message, message_store: MessageStore):
-
     try:
         chat_id = message.chat.id
         logging.info(f"Comando /resumo recebido no chat {chat_id}")
         temp_message = await message.answer("📝 Gerando um resumo...")
+
         # Carregar mensagens do chat_id
         messages = message_store.get_messages(chat_id)
         if not messages:
@@ -23,10 +23,20 @@ async def resumo_handler(message: types.Message, message_store: MessageStore):
 
         messages.sort(key=lambda msg: msg['timestamp'])
         messages_text = ''.join(f"{msg['sender_name']}: {msg['text']}\n" for msg in messages)
-        prompt = f"Resuma as seguintes mensagens de forma breve citando o autor de cada tópico, não faça textos muito longos: {messages_text[:6000]}"
+        prompt = f"Resuma as seguintes mensagens de forma breve citando o autor de cada tópico, não faça textos muito longos e não cite os autores repetidamente para cada mensagem, é só um resumo rápido das coisas comentadas nas mensagens: {messages_text[:6000]}"
+        logging.info(f"Prompt enviado ao modelo: {prompt}")
+
         response = model.generate_content(prompt)
         response_text = response.text
+        logging.info(f"Resposta do modelo: {response_text}")
+
+        if not response_text:
+            await message.answer("O modelo não retornou uma resposta.")
+            await message.bot.delete_message(chat_id=message.chat.id, message_id=temp_message.message_id)
+            return
+
         escaped_response_text = re.sub(r'([\.!*\-_~\[\](){}#+\-=\|\\><])', r'\\\1', response_text)
+        await message.answer(escaped_response_text, parse_mode='MarkdownV2')
         await message.bot.delete_message(chat_id=message.chat.id, message_id=temp_message.message_id)
 
     except Exception as e:
@@ -36,4 +46,3 @@ async def resumo_handler(message: types.Message, message_store: MessageStore):
 
 def register_resumo_handler(dp: Dispatcher, message_store: MessageStore):
     dp.register_message_handler(lambda message: resumo_handler(message, message_store), commands=['resumo'])
-
